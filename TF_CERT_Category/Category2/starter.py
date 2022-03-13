205ba69f923f1dd89ce7f574a6b5aa77e3987446
Han Lee <94848941+splinter9@users.noreply.github.com>
2022-03-13 22:49:19 +0900

    Update starter.py

--- TF_CERT_Category/Category2/starter.py
+++ TF_CERT_Category/Category2/starter.py
@@ -22,10 +22,65 @@
 # (28,28) as the input shape only. If you amend this, the tests will fail.
 #
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D,Flatten, Dropout
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+import time
 
 
 def solution_model():
     fashion_mnist = tf.keras.datasets.fashion_mnist
+
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    out_node = len(np.unique(y_train))
+
+    x_train = x_train / 255.
+    x_test = x_test / 255.
+
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    model = Sequential()
+    model.add(Conv1D(150, 2, activation='relu', input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(Flatten())
+    # model.add(LSTM(150,activation = 'relu', input_shape = (x_train.shape[1],x_train.shape[2])))
+    model.add(Dense(128))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(32, activation='relu'))
+    # model.add(Dropout(0.2))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(out_node, activation='softmax'))
+
+    opt = "adam"
+    model.compile(loss='categorical_crossentropy', optimizer=opt,
+                  metrics=['accuracy'])  # metrics=['accuracy'] 영향을 미치지 않는다
+    ########################################################################
+    # model.compile(loss = 'mse', optimizer = 'adam')
+    start = time.time()
+    from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+    import datetime
+    epoch = 100
+    patience_num = 5
+    date = datetime.datetime.now()
+    datetime = date.strftime("%m%d_%H%M")
+    filepath = "./_ModelCheckPoint/"
+    filename = '{epoch:04d}-{val_loss:4f}.hdf5'  # filepath + datetime
+    model_path = "".join([filepath, 'k35_cfar10_', datetime, "_", filename])
+    es = EarlyStopping(monitor='val_loss', patience=patience_num, mode='auto', restore_best_weights=True)
+    # mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto', verbose=1, save_best_only= True, filepath = model_path)
+    hist = model.fit(x_train, y_train, epochs=epoch, validation_split=0.2, callbacks=[es], batch_size=500)
+    end = time.time() - start
+    print('시간 : ', round(end, 2), '초')
+    ########################################################################
+
+    # 4 평가예측
+    loss = model.evaluate(x_test, y_test)
+    y_predict = model.predict(x_test)
+    print("loss : ", loss[0])
+    print("accuracy : ", loss[1])
 
     # YOUR CODE HERE
     return model
@@ -38,3 +93,8 @@ def solution_model():
 if __name__ == '__main__':
     model = solution_model()
     model.save("mymodel.h5")
+
+'''
+loss :  0.3532934784889221
+accuracy :  0.8830000162124634
+'''
