fa13fc59c61f3340dd9e4a2b0df7170a5333ab53
Han Lee <94848941+splinter9@users.noreply.github.com>
2022-03-13 23:01:50 +0900

    Update and rename starter.py to starter_sarcasm.py

--- TF_CERT_Category/Category4/starter_sarcasm.py
+++ TF_CERT_Category/Category4/starter_sarcasm.py
@@ -26,7 +26,7 @@ import numpy as np
 import urllib
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+from tensorflow.keras.callbacks import ReduceLROnPlateau,EarlyStopping
 
 def solution_model():
     url = 'https://storage.googleapis.com/download.tensorflow.org/data/sarcasm.json'
@@ -44,12 +44,51 @@ def solution_model():
     sentences = []
     labels = []
     # YOUR CODE HERE
+    with open('sarcasm.json', 'r') as f:
+        datasets = json.load(f)
+    for item in datasets:
+        sentences.append(item['headline'])
+        labels.append(item['is_sarcastic'])
+
+    x_train = np.array(sentences[:training_size])
+    x_test = np.array(sentences[training_size:])
+    y_train = np.array(labels[:training_size])
+    y_test = np.array(labels[training_size:])
+
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(x_train)
+
+    x_train = tokenizer.texts_to_sequences(x_train)
+    x_train = pad_sequences(x_train, maxlen=max_length, padding=padding_type,
+                            truncating=trunc_type)
+    x_test = tokenizer.texts_to_sequences(x_test)
+    x_test = pad_sequences(x_test, maxlen=max_length, padding=padding_type,
+                           truncating=trunc_type)
 
 
     model = tf.keras.Sequential([
     # YOUR CODE HERE. KEEP THIS OUTPUT LAYER INTACT OR TESTS MAY FAIL
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  input_length=max_length),
+        tf.keras.layers.Conv1D(32, 2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling1D(),
+        # tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.LSTM(128, activation='tanh', return_sequences=True),
+        tf.keras.layers.LSTM(64),
+        # tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(32),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(16),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['acc'])
+    es = EarlyStopping(monitor='val_acc', mode='max', patience=10)
+    lr = ReduceLROnPlateau(monitor='val_acc', patience=5, factor=0.1)
+    model.fit(x_train, y_train, epochs=10000, batch_size=32, validation_data=(x_test, y_test), callbacks=[lr, es])
+    print(model.evaluate(x_test, y_test))
     return model
 
 
