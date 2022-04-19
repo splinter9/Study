0599c0783b8ad7dbf0991ec9cb5700aa44238964
Han Ericsson Lee <94848941+splinter9@users.noreply.github.com>
2022-04-19 18:00:16 +0900

    Update m02_1_and.py

--- ML/m02_1_and.py
+++ ML/m02_1_and.py
@@ -3,21 +3,23 @@ from sklearn.svm import LinearSVC
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 
-#1. DATA
+# 1. DATA
 x_data = [[0,0],[0,1],[1,0],[1,1]]
 y_data = [0,0,0,1]
 
 # [0,0] -> [0] 
 # [0,1] -> [0] ... [1,1] ->[1]
 
-#2. MODEL
-#model = LinearSVC()
+# 2. MODEL
+# model = LinearSVC()
 model = Perceptron()
 
-#3. FIT
+
+# 3. FIT
 model.fit(x_data, y_data)
 
-#4. PREDICT
+
+# 4. PREDICT
 y_predict = model.predict(x_data)
 print(x_data, "의 예측결과: ", y_predict)
 results = model.score(x_data, y_data)
